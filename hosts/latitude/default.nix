e650b3e267a1c12212df9b35a341e2bf3d463d04
Raoul <raoul.sidler@vgt.energy>
2025-12-15 14:09:03 +0100

    updatet latitude

---
+++ hosts/latitude/default.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader Setup
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Timezone
+  time.timeZone = "Europe/Amsterdam";
+
+  # User Account (Passwörter/Gruppen)
+  # Home Manager kümmert sich um die Apps, aber der User muss existieren
+  users.users.raoul = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; 
+    packages = with pkgs; [ tree ];
+  };
+
+  # HIER ist der saubere Teil:
+  # Wir aktivieren nur noch die Module. Woher die kommen,
+  # regelt die Flake via modules/system/default.nix.
+  modules.system = {
+    keyboard.enable = true;
+    gnome.enable = true;
+    maintenance.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+  ];
+
+  # Experimental features (Flakes) sind immer gut
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "24.11";
+}
--- hosts/latitude/home.nix
+++ hosts/latitude/home.nix
@@ -5,28 +5,19 @@
   home.stateVersion = "24.11"; 
  
   programs.home-manager.enable = true;
- 
-  imports = [
-	../../modules/users/git/default.nix
-	../../modules/users/brave/default.nix
-	../../modules/users/bitwarden/default.nix
-	../../modules/users/vscode/default.nix
-	../../modules/users/geary/default.nix
-  	../../modules/users/docker/default.nix
-	../../modules/users/discord/default.nix
-	../../modules/users/claude/default.nix
-  ];
   
   modules.home = {
-	git = { enable = true; userName = "Raoul"; userEmail = "raoul.sidler@gmx.ch"; };
+	git = { enable = true; userName = "Raoul"; userEmail = "raoul.sidler@vgt.energy"; };
 	brave.enable = true;
 	bitwarden.enable = true;
 	vscode.enable = true;
 	geary.enable = true;
-	docker.enable = true;
 	discord.enable = true;
-	# claude.enable = true;
+	neovim.enable = true;
+	ghostty.enable = true;
+
   };
+
 
 	
 }
