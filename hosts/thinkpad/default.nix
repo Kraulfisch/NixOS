498b8d9d49ec69335b17869bd452c6514a4c2a74
Raoul <raoul.sidler@gmx.ch>
2026-02-14 17:37:28 +0100

    added thinkpad

---
+++ hosts/thinkpad/default.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader Setup
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.enableAllFirmware = true;
+
+
+  # Timezone
+  time.timeZone = "Europe/Amsterdam";
+
+  # User Account (Passwörter/Gruppen)
+  # Home Manager kümmert sich um die Apps, aber der User muss existieren
+  users.users.raoul = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; 
+    packages = with pkgs; [ tree ];
+  };
+  modules.system = {
+    keyboard.enable = true;
+    gnome.enable = true;
+    maintenance.enable = true;
+    openwebui.enable = true;
+    ollama.enable = true;
+#    logitech.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    wget
+  ];
+
+  environment.sessionVariables = {
+	GTK_IM_MODULE = "simple";
+  };
+  # Experimental features (Flakes) sind immer gut
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "24.11";
+}
